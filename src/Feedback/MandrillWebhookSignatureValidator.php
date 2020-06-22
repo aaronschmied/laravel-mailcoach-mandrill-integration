b351cbd2266eed0f3b07700783755f1a7620cca5
Aaron Schmied <aaron@schmied.dev>
2020-06-22 18:55:42 +0200

    Created the feedback validator according to the mandrill docs.

---
+++ src/Feedback/MandrillWebhookSignatureValidator.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace SchmiedDev\MailcoachMandrillIntegration\Feedback;
+
+use Illuminate\Http\Request;
+use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
+use Spatie\WebhookClient\WebhookConfig;
+
+class MandrillWebhookSignatureValidator implements SignatureValidator
+{
+    /**
+     * Get the requests content for the signature validation.
+     *
+     * @param Request $request
+     *
+     * @return string
+     */
+    protected function getRequestPayloadForSigning(Request $request)
+    {
+        $url = route('mandrillFeedback');
+
+        $signedData = $url;
+
+        $params = $request->toArray();
+
+        ksort($params);
+
+        foreach ($params as $key => $value) {
+            $signedData .= $key;
+            $signedData .= $value;
+        }
+
+        return $signedData;
+    }
+
+    /**
+     * Validate the signature for the given request.
+     *
+     * @param Request       $request
+     * @param WebhookConfig $config
+     *
+     * @return bool
+     */
+    public function isValid(Request $request, WebhookConfig $config): bool
+    {
+        $requestSignature = $request->header($config->signatureHeaderName);
+
+        $generatedSignature = base64_encode(
+            hash_hmac(
+                'sha1',
+                $this->getRequestPayloadForSigning($request),
+                $config->signingSecret,
+                true
+            )
+        );
+
+        return hash_equals($generatedSignature, $requestSignature);
+    }
+}
